06399b149199ae2cedc340c0a97e6bca024a65ce
EleonoraTitarenko <yaroslavnaella@gmail.com>
2019-12-16 16:40:17 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,21 @@
-var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+
+let navMain = document.querySelector('.main-nav')
+    navToggle = document.querySelector('.main-nav__toggle')
+    pageheader = document.querySelector('.page-header')
+    some = false;
 
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
+    some = !some
+    console.log(some)
+    if(some) {
+      pageheader.classList.remove('page-header--open');
+      pageheader.classList.add('page-header--close');
+    } else {
+      pageheader.classList.remove('page-header--close');
+      pageheader.classList.add('page-header--open');
+    }
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
@@ -12,3 +24,11 @@ var navMain = document.querySelector('.main-nav');
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+  // function some() {
+  //   if(some) {
+  //     pageheader.classList.add('page-header--open');
+  //   } else {
+  //     pageheader.classList.add('page-header--close');
+  //   }
+  // }
