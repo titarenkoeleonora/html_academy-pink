44e2e87e9b43b9fc297ae3c4adfa4d71f95470b6
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-01-14 12:37:26 +0300

    //

--- gulpfile.js
+++ gulpfile.js
@@ -58,9 +58,9 @@ gulp.task("sprite", function () {
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
-    // .pipe(posthtml([
-    //   include()
-    // ]))
+    .pipe(posthtml([
+      include()
+    ]))
     .pipe(gulp.dest("build"));
 })
 
