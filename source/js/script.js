87cf421737630af6a96a8004bc955735b2b12d20
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-01-15 09:17:12 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,25 +2,25 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var pageheader = document.querySelector('.page-header');
-var some = false;
+var colorMenu = false;
 
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
-    some = !some
-    console.log(some)
-    if(some) {
-      pageheader.classList.remove('page-header--open');
-      pageheader.classList.add('page-header--close');
-    } else {
-      pageheader.classList.remove('page-header--close');
-      pageheader.classList.add('page-header--open');
-    }
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
+    }
+    colorMenu = !colorMenu
+    console.log(colorMenu)
+    if(colorMenu) {
+      pageheader.classList.remove('page-header--open');
+      pageheader.classList.add('page-header--close');
+    } else {
+      pageheader.classList.remove('page-header--close');
+      pageheader.classList.add('page-header--open');
     }
   });
