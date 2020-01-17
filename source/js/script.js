477b6dd81368eeb1197a5a07a6a655ff4a56d656
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-01-18 01:08:11 +0300

    Правки. ПП с новыми брейкпоинтами, мелкие доделки

--- source/js/script.js
+++ source/js/script.js
@@ -5,23 +5,23 @@ var pageheader = document.querySelector('.page-header');
 var colorMenu = false;
 
   navMain.classList.remove('main-nav--nojs');
-  navMain.classList.add('main-nav--opened');
+  navMain.classList.add('main-nav--closed');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
+    if (navMain.classList.contains('main-nav--opened')) {
       navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+    } else {
+      navMain.classList.add('main-nav--opened');
+      navMain.classList.remove('main-nav--closed');
     }
     colorMenu = !colorMenu
     console.log(colorMenu)
     if(colorMenu) {
-      pageheader.classList.remove('page-header--open');
-      pageheader.classList.add('page-header--close');
-    } else {
       pageheader.classList.remove('page-header--close');
       pageheader.classList.add('page-header--open');
+    } else {
+      pageheader.classList.remove('page-header--open');
+      pageheader.classList.add('page-header--close');
     }
   });
